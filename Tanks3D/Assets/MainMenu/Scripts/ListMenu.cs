af023bbbcfbe6f2e0ca01bfbf1b512993ad42987
Ekaterina <95909640+VEkaterinaA@users.noreply.github.com>
2022-07-08 16:52:59 +0300

    Tanks3D
    Change Main menu.
    Menu in the form of a list of buttons

---
+++ Tanks3D/Assets/MainMenu/Scripts/ListMenu.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ListMenu : MonoBehaviour
+{
+    private Button Button_Template;
+
+
+    private void Start()
+    {
+        Button_Template = transform.GetChild(0).GetComponent<Button>();
+        ListButtonMenu();
+    }
+
+    void ListButtonMenu()
+    {
+        AddRowMenu("Start game", StartGame_Click);
+        AddRowMenu("Exit", ExitGame_Click);
+
+        Destroy(Button_Template.gameObject);
+    }
+    void AddRowMenu(string text, UnityEngine.Events.UnityAction ClickButton_method)
+    {
+        Button AddButton = Instantiate(Button_Template, transform);
+        AddButton.transform.SetSiblingIndex(0);
+        AddButton.transform.GetChild(0).GetComponent<Text>().text = text;
+        AddButton.onClick.AddListener(ClickButton_method);
+
+    }
+
+    void StartGame_Click()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void ExitGame_Click()
+    {
+        Application.Quit();
+    }
+}
+
+
--- Tanks3D/Assets/MainMenu/Scripts/MainMenuController.cs
+++ Tanks3D/Assets/MainMenu/Scripts/MainMenuController.cs
@@ -7,21 +7,6 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    public Button StartGame, Exit;
-
-    private void Awake()
-    {
-        StartGame.onClick.AddListener(StartGameClick);
-        Exit.onClick.AddListener(ExitClick);
-    }
-
-    private void StartGameClick()//first level
-    {
-        SceneManager.LoadScene(1);
-    }
-    private void ExitClick()
-    {
-        Application.Quit();
-    }
+   
 
 }
